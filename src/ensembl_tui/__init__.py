27d2b98fee066f2f104332a9f115d6f7cdb9ecd3
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-03-25 16:24:48 +1100

    MAINT: stop annoying warnings from loky
    [CHANGED] these are more likely coming from cogent3 than this project

--- src/ensembl_tui/__init__.py
+++ src/ensembl_tui/__init__.py
@@ -4,6 +4,7 @@ from warnings import filterwarnings
 
 filterwarnings("ignore", message=".*MPI")
 filterwarnings("ignore", message="Can't drop database.*")
+filterwarnings("ignore", message="A worker stopped while some jobs.*")
 
 
 __version__ = "0.1a9"
