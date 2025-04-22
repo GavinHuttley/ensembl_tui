9afd14022ae11b14fa1bcf3ebc702e8fe5daef82
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-04-23 08:58:09 +1000

    ENH: added --ref_genes option to the homologs command, fixes #168
    [NEW] option to provide a tab delimited file with a stableid column
        instead of querying.

--- src/ensembl_tui/cli.py
+++ src/ensembl_tui/cli.py
@@ -6,7 +6,7 @@ from collections.abc import Mapping
 
 import click
 import trogon
-from cogent3 import get_app, load_table, open_data_store
+from cogent3 import get_app, open_data_store
 from scitrack import CachingLogger
 
 from ensembl_tui import __version__
@@ -322,6 +322,7 @@ def compara_summary(installed: pathlib.Path) -> None:
     help="type of homology",
 )
 @cli_opt.ref
+@cli_opt.ref_genes
 @cli_opt.coord_names
 @cli_opt.nprocs
 @cli_opt.limit
@@ -332,6 +333,7 @@ def homologs(
     outdir: pathlib.Path,
     homology_type: str,
     ref: str,
+    ref_genes: list[str] | None,
     coord_names: str,
     num_procs: int,
     limit: int,
@@ -347,6 +349,13 @@ def homologs(
     if ref is None:
         eti_util.print_colour(
             text="ERROR: a reference species name is required, use --ref",
+            colour="red",
+        )
+        sys.exit(1)
+
+    if ref_genes and coord_names:
+        eti_util.print_colour(
+            text="ERROR: cannot specify both ref_genes and coord_names",
             colour="red",
         )
         sys.exit(1)
@@ -368,16 +377,17 @@ def homologs(
 
     # we don't use the limit argument for this query since we want the limit
     # to be the number of homology matches
-    gene_ids = list(
-        genome.get_ids_for_biotype(
-            biotype="protein_coding",
-            seqid=coord_names,
-        ),
-    )
+    if not ref_genes:
+        ref_genes = list(
+            genome.get_ids_for_biotype(
+                biotype="protein_coding",
+                seqid=coord_names,
+            ),
+        )
 
     if verbose:
         eti_util.print_colour(
-            text=f"Found {len(gene_ids):,} gene IDs for {ref!r}",
+            text=f"Found {len(ref_genes):,} gene IDs for {ref!r}",
             colour="yellow",
         )
 
@@ -393,10 +403,10 @@ def homologs(
         progress.TimeElapsedColumn(),
     ) as progress:
         searching = progress.add_task(
-            total=limit or len(gene_ids),
+            total=limit or len(ref_genes),
             description="Homolog search",
         )
-        for gid in gene_ids:
+        for gid in ref_genes:
             if rel := db.get_related_to(gene_id=gid, relationship_type=homology_type):
                 related.append(rel)
                 progress.update(searching, advance=1)
@@ -404,7 +414,7 @@ def homologs(
             if limit and len(related) >= limit:
                 break
 
-        progress.update(searching, advance=len(gene_ids))
+        progress.update(searching, advance=len(ref_genes))
 
         if verbose:
             eti_util.print_colour(
