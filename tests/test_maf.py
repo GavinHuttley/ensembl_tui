bca9dc3b6b664b0b6582cfc9fdcd45c42019125f
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-07-28 12:16:03 +1000

    TST: additional maf file test

--- tests/test_maf.py
+++ tests/test_maf.py
@@ -1,5 +1,7 @@
 import pytest
 
+from ensembl_tui import _config as eti_config
+from ensembl_tui import _genome as eti_genome
 from ensembl_tui import _maf as eti_maf
 
 
@@ -29,3 +31,26 @@ def test_process_maf_line_plus(line):
     # maf is zero based
     assert n.start == 2
     assert n.stop == 2 + 7
+
+
+def test_process_maf_line_minus():
+    from cogent3 import DNA
+
+    seq_plus = "ACCTTTTGGGGGG"
+    seq_minus = "CCCCCCAAAAGGT"
+    # just check our rc is correct
+    assert seq_plus == DNA.rc(seq_minus)
+    #            01234567890123
+    maf_start = 6
+    maf_size = 4
+    maf_line = f"s hg38.chr22 {maf_start} {maf_size} - {len(seq_minus)} {seq_minus}"
+    n, s = eti_maf.process_maf_line(maf_line)
+    expected_start = seq_plus.find("T")
+    expected_stop = expected_start + maf_size
+    assert n.start == expected_start
+    assert n.stop == expected_stop
+    assert seq_plus[n.start : n.stop] == DNA.rc(
+        seq_minus[maf_start : maf_start + maf_size],
+    )
+
+
