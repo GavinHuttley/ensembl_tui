3f90e96837d53cb6aaf21c769d6c2163fa21f197
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-09-17 12:45:46 +1000

    DEV: extra info in display

--- noxfile.py
+++ noxfile.py
@@ -6,6 +6,7 @@ _py_versions = range(10, 14)
 @nox.session(python=[f"3.{v}" for v in _py_versions])
 def test(session):
     session.install("-e.[test]")
+    session.run("pip", "list")
     session.chdir("tests")
     session.run(
         "pytest",
