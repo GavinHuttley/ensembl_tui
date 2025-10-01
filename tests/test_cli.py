d40f2ca1c57734e0fa4ec8035174e0573de64ef3
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-10-01 18:10:24 +1000

    MAINT: add ensembl release info to output from installed command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -84,6 +84,7 @@ def test_installed(installed):
     assert sum(f.stat().st_size for f in config.homologies_path.iterdir()) > 8_000
     r = RUNNER.invoke(eti_cli.installed, [f"-i{installed}"], catch_exceptions=False)
     assert r.exit_code == 0, r.output
+    assert "Ensembl release:" in r.output
     assert "Installed genomes" in r.output
     assert "caenorhabditis_elegans" in r.output
     path = config.installed_genome("caenorhabditis_elegans")
