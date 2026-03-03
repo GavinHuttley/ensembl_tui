001aa93b7ded2160b0fc3ea23f33ae6abc6d32fa
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2026-03-03 12:45:28 +1100

    DEV: bumped version to 0.7.6

--- src/ensembl_tui/__init__.py
+++ src/ensembl_tui/__init__.py
@@ -6,4 +6,4 @@ filterwarnings("ignore", message=".*MPI")
 filterwarnings("ignore", message="Can't drop database.*")
 filterwarnings("ignore", message="A worker stopped while some jobs.*")
 
-__version__ = "0.7.5"
+__version__ = "0.7.6"
